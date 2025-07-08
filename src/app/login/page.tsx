c6a2790888b5596758adf2648bfb68eb6f02ca3c
Saksham Tripoathi <123883320+Dashanand10@users.noreply.github.com>
2025-07-08 23:17:52 +0530

    Fix ESLint errors in authentication and chat components

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -90,7 +90,7 @@ export default function LoginPage() {
         </form>
 
         <p className="mt-4 text-center text-sm text-gray-600">
-          Don't have an account?{' '}
+          Don&apos;t have an account?{' '}
           <Link href="/signup" className="text-indigo-600 hover:underline">
             Sign up
           </Link>
